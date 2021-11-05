16c008f96b8f108997b2f2e1b11a7099e00136db
Byungkyu Jun <32653425+coxozo123@users.noreply.github.com>
2021-11-05 14:45:35 +0900

    added multiple inheritance example

---
+++ CppExamples/MultipleInheritance.cpp
@@ -0,0 +1,65 @@
+#include "MultipleInheritance.h"
+
+#include <memory>
+
+void main_MultipleInheritanceExample()
+{
+	/// CALL PROCEDURE AFFECTED BY INHERITANCE ORDER
+	///
+	/// in case of : public parent 1 class, parent 2 class
+	std::cout << "--- call procedure affected by inheritance order ---" << std::endl;
+	{
+		/// calling order
+		/// 
+		/// 1. parent 1 class constructor
+		/// 2. parent 2 class constructor
+		/// 3. child class constructor
+		/// 4. child class destructor
+		/// 5. parent 2 class destructor
+		/// 6. parent 1 class destructor
+		std::unique_ptr<MultipleInheritanceExampleClass_Child> exampleClass = std::make_unique<MultipleInheritanceExampleClass_Child>();
+	}
+
+	/// POLYMORPHISM WITH INDICATING VIRTUAL TABLE
+	///
+	std::cout << "--- polymorphism ---" << std::endl;
+	/// initiating with parent 1
+	/// 
+	/// exampleClass's virtual table pointer address -> child class's virtual table
+	{
+		std::unique_ptr<MultipleInheritanceExampleClass_Parent_1> exampleClass = std::make_unique<MultipleInheritanceExampleClass_Child>();
+	}
+	/// initiating with parent 2
+	/// 
+	/// compiler error 
+	/// can not instanciate with secondly inherited base class
+	{
+		//std::unique_ptr<MultipleInheritanceExampleClass_Parent_2> exampleClass = std::make_unique<MultipleInheritanceExampleClass_Child>();
+	}
+	/// initiating with child
+	/// 
+	/// exampleClass's virtual table pointer address -> child class's virtual table
+	{
+		std::unique_ptr<MultipleInheritanceExampleClass_Child> exampleClass = std::make_unique<MultipleInheritanceExampleClass_Child>();
+	}
+
+
+	/// SIZE OF DERIVED CLASS
+	///
+	std::cout << "--- size of derived class ---" << std::endl;
+	/// size of parent 1 class
+	{
+		std::unique_ptr<MultipleInheritanceExampleClass_Parent_1> exampleClass = std::make_unique<MultipleInheritanceExampleClass_Parent_1>();
+		std::cout << "size of base 1 class : " << sizeof(*exampleClass) << std::endl;
+	}
+	/// size of parent 2 class
+	{
+		std::unique_ptr<MultipleInheritanceExampleClass_Parent_2> exampleClass = std::make_unique<MultipleInheritanceExampleClass_Parent_2>();
+		std::cout << "size of base 2 class : " << sizeof(*exampleClass) << std::endl;
+	}
+	/// size of derived class
+	{
+		std::unique_ptr<MultipleInheritanceExampleClass_Child> exampleClass = std::make_unique<MultipleInheritanceExampleClass_Child>();
+		std::cout << "size of derived class : " << sizeof(*exampleClass) << std::endl;
+	}
+}
---
+++ CppExamples/MultipleInheritance.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <iostream>
+
+/// ********************
+/// MULTIPLE INHERITANCE
+/// ********************
+
+/// case of using mutiple inheritance architecture
+///
+/// 1. child classes must have several base classes' properties
+/// 2. for flexible architecture, base classes are fractionated by abstract, interface, implemented
+
+/// Multiple Inheritance Example Parent Class 1
+class MultipleInheritanceExampleClass_Parent_1
+{
+public:
+	MultipleInheritanceExampleClass_Parent_1() : mNum_Parent_1 { 0 }
+	{
+		std::cout << "parent 1 class constructor called" << std::endl;
+	}
+
+	virtual ~MultipleInheritanceExampleClass_Parent_1() noexcept
+	{
+		std::cout << "parent 1 class destructor called" << std::endl;
+	}
+
+	int mNum_Parent_1;
+};
+
+/// Multiple Inheritance Example Parent Class 2
+class MultipleInheritanceExampleClass_Parent_2
+{
+public:
+	MultipleInheritanceExampleClass_Parent_2() : mNum_Parent_2 { 0 } 
+	{
+		std::cout << "parent 2 class constructor called" << std::endl;
+	}
+
+	virtual ~MultipleInheritanceExampleClass_Parent_2() noexcept
+	{
+		std::cout << "parent 2 class destructor called" << std::endl;
+	}
+
+	int mNum_Parent_2;
+};
+
+/// Multiple Inheritance Example Child Class
+///
+/// Inheritancing order affects call procedure of constructors and destructors
+/// 
+/// child class's size is sum of
+/// 1. base classes' virtual table address (4bytes on 32bit os, 8bytes on 64bit os)
+/// 2. base classes' member variable size
+/// 3. child class's virtual table address (4bytes on 32bit os, 8bytes on 64bit os)
+/// 4. child class's member variable size
+class MultipleInheritanceExampleClass_Child : public MultipleInheritanceExampleClass_Parent_1, MultipleInheritanceExampleClass_Parent_2
+											// public MultipleInheritanceExampleClass_Parent_2, MultipleInheritanceExampleClass_Parent_1
+{
+public:
+	MultipleInheritanceExampleClass_Child() : mNum_Child { 0 }
+	{
+		std::cout << "child class constructor called" << std::endl;
+	}
+
+	~MultipleInheritanceExampleClass_Child() noexcept
+	{
+		std::cout << "child class destructor called" << std::endl;
+	}
+
+	int mNum_Child;
+};
+
+void main_MultipleInheritanceExample();
