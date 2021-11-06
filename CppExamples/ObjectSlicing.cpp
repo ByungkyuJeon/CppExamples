9ccd297e798b99a916d1590c0f4b2746ceaf27e0
Byungkyu Jun <32653425+coxozo123@users.noreply.github.com>
2021-11-07 00:51:32 +0900

    added object slicing example

---
+++ CppExamples/ObjectSlicing.cpp
@@ -0,0 +1,57 @@
+#include "ObjectSlicing.h"
+
+#include <memory>
+
+void main_ObjectSlicingExample()
+{
+	/// OBJECT SLICING EXAMPLE
+	///
+	/// when child class instance is copied to its parent class, appearing child class's data loss is called object slicing
+	/// 
+	/// loss of data
+	/// 1. virtual table address
+	/// 2. child class data
+	/// 
+	/// solution to object slicing problem
+	/// 1. deleting copy assignment function in base class, qualify copy constructor to 'protected' - blocks
+	/// 2. 
+	{
+		ObjectSlicingExampleClass_Child exampleClass_Child{ 9 };
+		exampleClass_Child.Print();
+
+		/// gets child instance refernce to parent type
+		/// object slicing does not occur
+		ObjectSlicingExampleClass_Parent_Sliced& exampleClass_ParentRef = exampleClass_Child;
+
+		/// parent class's copy constructor is called
+		/// object slicing occurred
+		ObjectSlicingExampleClass_Parent_Sliced exampleClass_ParentVal_Sliced = exampleClass_Child;
+		exampleClass_ParentVal_Sliced.Print();
+
+		/// SOLUTION 1
+		/// 
+		/// parent class's copy constructor can not be called
+		/// object slicing can not occur (compiler error)
+		/// 
+		/// instead, call child class's copy constructor
+		/// 
+		std::cout << "--- solution 1 ---" << std::endl;
+		// ObjectSlicingExampleClass_Parent_Blocked exampleClass_parentVal_Blocked = exampleClass_Child; // COMPILER ERROR
+		ObjectSlicingExampleClass_Child exampleClass_CopiedByConstructor{ exampleClass_Child };
+		exampleClass_CopiedByConstructor.Print();
+		// size of copied class
+
+		/// SOLUTION 2
+		///
+		/// 
+		/// parent class's copy constructor can not be called
+		/// object slicing can not occur (compiler error)
+		/// 
+		/// instead, explicitly implement 'clone()' function and copy from that
+		
+		//ObjectSlicingExampleClass_Parent_Blocked exampleClass_CopiedByClone = exampleClass_Child.Clone();
+	}
+
+
+
+}
---
+++ CppExamples/ObjectSlicing.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+
+class ObjectSlicingExampleClass_Parent_Sliced
+{
+public:
+	ObjectSlicingExampleClass_Parent_Sliced() :mNum_Parent{ 0 } {}
+	ObjectSlicingExampleClass_Parent_Sliced(int num) : mNum_Parent{ num } {}
+
+	virtual ~ObjectSlicingExampleClass_Parent_Sliced() noexcept = default;
+	
+	virtual void Print()
+	{
+		std::cout << "parent class print called, member variable value : " << mNum_Parent << std::endl;
+	}
+
+	int mNum_Parent;
+};
+
+class ObjectSlicingExampleClass_Parent_Blocked
+{
+public:
+
+	ObjectSlicingExampleClass_Parent_Blocked() : mNum_Parent{ 0 } {}
+	ObjectSlicingExampleClass_Parent_Blocked(int num) : mNum_Parent{ num } {}
+
+	// copy assignment block
+	ObjectSlicingExampleClass_Parent_Blocked& operator=(ObjectSlicingExampleClass_Parent_Blocked other) = delete;
+
+	virtual ~ObjectSlicingExampleClass_Parent_Blocked() noexcept = default;
+
+	// explicit copy function
+	virtual std::unique_ptr<ObjectSlicingExampleClass_Parent_Blocked> Clone() { return nullptr; }
+
+	int mNum_Parent;
+
+protected:
+
+	// copy constructor block
+	ObjectSlicingExampleClass_Parent_Blocked(const ObjectSlicingExampleClass_Parent_Blocked& other) = default;
+};
+
+class ObjectSlicingExampleClass_Child : public ObjectSlicingExampleClass_Parent_Sliced
+{
+public:
+
+	ObjectSlicingExampleClass_Child() : mNum_Child{ 0 } {}
+	ObjectSlicingExampleClass_Child(int num) : mNum_Child{ num } {}
+
+	void Print() override
+	{
+		std::cout << "child class print called, parent variable value : " 
+			<< mNum_Parent 
+			<< " / member variable value : "
+			<< mNum_Child 
+			<< std::endl;
+	}
+
+private:
+	
+	int mNum_Child;
+};
+
+
+void main_ObjectSlicingExample();
