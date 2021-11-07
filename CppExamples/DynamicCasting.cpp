1c711000ab95cd72d5c938980e30271617aea293
Byungkyu Jun <32653425+coxozo123@users.noreply.github.com>
2021-11-07 16:14:10 +0900

    added dynamic casting example

---
+++ CppExamples/DynamicCasting.cpp
@@ -0,0 +1,65 @@
+#include "DynamicCasting.h"
+
+#include <memory>
+
+void main_DynamicCastingExample()
+{
+	/// STATIC CASTING
+	std::cout << "--- static casting ---" << std::endl;
+	{
+		DynamicCastingExampleClass_Child_1 exampleClass = DynamicCastingExampleClass_Child_1();
+		exampleClass.Print();
+
+		/// upcasting
+		///
+		/// static casing
+		auto upCasting = static_cast<DynamicCastingExampleClass_Parent&>(exampleClass);
+		/// implicit casting
+		upCasting = exampleClass;
+		upCasting.Print();
+
+		/// downcasting
+		auto downCasting = static_cast<DynamicCastingExampleClass_Child_1&>(upCasting);
+		downCasting.Print();
+
+		/// downcasting to wrong child
+		///
+		/// complies success for this example class, but might occur crucial error or bug
+		/// because child 2 class data is null
+		auto downCasting_Other = static_cast<DynamicCastingExampleClass_Child_2&>(upCasting);
+		downCasting_Other.Print();
+	}
+
+	/// DYNAMIC CASTING
+	///
+	/// dynamic casting looks up class's virtual table for type info
+	/// if casting type doesn't match to class type, returns nullptr
+	/// 
+	/// many of cpp projects restricts using dynamic_cast
+	/// because there is more proper way of class structure without using dynamic_cast
+	/// using dynamic_cast means that software architecture or class structure(Inheritance) is in wrong way
+	std::cout << "--- dynamic casting ---" << std::endl;
+	{
+		DynamicCastingExampleClass_Parent* exampleClass =  new DynamicCastingExampleClass_Child_1();
+		exampleClass->Print();
+		auto downCasting_Right = dynamic_cast<DynamicCastingExampleClass_Child_1*>(exampleClass);
+		if (downCasting_Right == nullptr)
+		{
+			std::cout << "dynamic casting failed" << std::endl;
+		}
+		else
+		{
+			downCasting_Right->Print();
+		}
+		auto downCasting_Wrong = dynamic_cast<DynamicCastingExampleClass_Child_2*>(exampleClass);
+		if (downCasting_Wrong == nullptr)
+		{
+			std::cout << "dynamic casting failed" << std::endl;
+		}
+		else
+		{
+			downCasting_Wrong->Print();
+		}
+
+	}
+}
---
+++ CppExamples/DynamicCasting.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <iostream>
+
+class DynamicCastingExampleClass_Parent
+{
+public:
+
+	DynamicCastingExampleClass_Parent() = default;
+
+	virtual ~DynamicCastingExampleClass_Parent() noexcept = default;
+
+	virtual void Print()
+	{
+		std::cout << "parent class print called" << std::endl;
+	}
+
+};
+
+class DynamicCastingExampleClass_Child_1 : public DynamicCastingExampleClass_Parent
+{
+public:
+
+	DynamicCastingExampleClass_Child_1() = default;
+	~DynamicCastingExampleClass_Child_1() = default;
+
+	void Print() override
+	{
+		std::cout << "child class 1 print called" << std::endl;
+	}
+};
+
+class DynamicCastingExampleClass_Child_2 : public DynamicCastingExampleClass_Parent
+{
+public:
+
+	DynamicCastingExampleClass_Child_2() = default;
+	~DynamicCastingExampleClass_Child_2() = default;
+
+	void Print() override
+	{
+		std::cout << "child class 2 print called" << std::endl;
+	}
+};
+
+void main_DynamicCastingExample();
