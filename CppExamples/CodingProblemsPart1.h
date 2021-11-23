c375ff427bb950565573d1484e6988c4e26d6c2e
Byungkyu Jun <32653425+coxozo123@users.noreply.github.com>
2021-11-24 07:24:31 +0900

    added problem 3

--- CppExamples/CodingProblemsPart1.h
+++ CppExamples/CodingProblemsPart1.h
@@ -3,9 +3,10 @@
 #include <iostream>
 #include <vector>
 #include <array>
+#include <string>
 #include "TimeChecker.h"
 
-/// 문제 : 당신은 음식점의 계산을 도와주는 점원입니다. 카운터에는 거스름돈으로 사용할 500원, 100원, 50원, 10원짜리
+/// 문제 1 : 당신은 음식점의 계산을 도와주는 점원입니다. 카운터에는 거스름돈으로 사용할 500원, 100원, 50원, 10원짜리
 /// 동전이 무한히 존재한다고 가정합니다. 손님에게 거슬러 주어야 할 돈이 N원일 때 거슬러 주어야 할 동전의 최소 개수를 
 /// 구하세요. 단, 거슬러 줘야 할 돈 N은 항상 10의 배수입니다.
 /// 
@@ -34,7 +35,7 @@ void Problem_1()
 	std::cout << "Consumed Time : " << consumedTime << " ms" << std::endl;
 }
 
-/// 어떠한 수 N이 1이 될 대까지 다음의 두 과정 중 하나를 반복적으로 선택하여 수행하려고 합니다. 단, 두 번째 연산은 
+/// 문제 2 : 어떠한 수 N이 1이 될 대까지 다음의 두 과정 중 하나를 반복적으로 선택하여 수행하려고 합니다. 단, 두 번째 연산은 
 /// N이 K로 나누어 떨어질 때만 선택할 수 있습니다.
 /// 1. N에서 1을 뺍니다.
 /// 2. N을 K로 나눕니다.
@@ -154,11 +155,88 @@ void Problem_2_Efficient()
 	std::cout << "consumed time : " << consumedTime << " ms" << std::endl;
 }
 
+/// 문제 3 : 각 자리가 숫자(0부터 9)로만 이루어진 문자열 S가 주어졌을 때, 왼쪽부터 오른쪽으로 하나씩 모든 숫자를 
+/// 확인하며 숫자 사이에 'x' 혹은 '+' 연산자를 넣어 결과적으로 만들어질 수 있는 가장 큰 수를 구하는 프로그램을
+/// 작성하세요. 단, +보다 x를 먼저 계산하는 일반적인 방식과는 달리, 모든 연산은 왼쪽에서부터 순서대로 이루어진다고 가정합니다.
+/// 
+/// 예를 들어 02984라는 문자열로 만들 수 있는 가장 큰 수는 ((((0 + 2) x 9) x 8) x 4) = 576입니다.
+/// 또한 만들어질 수 있는 가장 큰 수는 항상 20억 이하의 정수가 되도록 입력이 주어집니다.
+void Problem_3_Trial_1()
+{
+	std::string outStr;
+
+	std::cin >> outStr;
+
+	double consumedTime = TimeChecker::CheckTime([&] {
+
+		std::vector<int> nums;
+
+		for (const auto& elem : outStr)
+		{
+			nums.emplace_back(elem - '0');
+		}
+
+		int result = nums[0];
+
+		for (size_t idx = 1; idx < nums.size(); idx++)
+		{
+			if (result != 0 && nums[idx] != 0)
+			{
+				result *= nums[idx];
+			}
+			else
+			{
+				result += nums[idx];
+			}
+		}
+
+		std::cout << result << std::endl;
+	});
+
+	std::cout << "consumed time : " << consumedTime << " ms" << std::endl;
+}
+
+/// remove making vector container
+/// number 1 has to be considered also
+void Problem_3_Final()
+{
+	std::string outStr;
+
+	std::cin >> outStr;
+
+	double consumedTime = TimeChecker::CheckTime([&] {
+		
+		int result = outStr[0] - '0';
+
+		for (size_t idx = 1; idx < outStr.size(); idx++)
+		{
+			if (result > 1 && outStr[idx] > 1)
+			{
+				result *= outStr[idx] - '0';
+			}
+			else
+			{
+				result += outStr[idx] - '0';
+			}
+		}
+
+		std::cout << result << std::endl;
+
+	});
+
+	std::cout << "consumed time : " << consumedTime << " ms" << std::endl;
+}
+
 void main_CodingProblemsPart1()
 {
 	/// Greedy Problems
 
 	//Problem_1();
-	Problem_2_InEfficient();
-	Problem_2_Efficient();
+	
+	//Problem_2_InEfficient();
+	//Problem_2_Efficient_Trial_1()
+	//Problem_2_Efficient();
+
+	//Problem_3_Trial_1();
+	//Problem_3_Final();
 }
