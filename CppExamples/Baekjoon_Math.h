5e9cbb0b954b615a46892f1445986c10cfdd2146
Byungkyu Jun <32653425+coxozo123@users.noreply.github.com>
2021-12-01 22:53:54 +0900

    added baekjoon 4344

--- CppExamples/Baekjoon_Math.h
+++ CppExamples/Baekjoon_Math.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
 
 /// n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
 void Baekjoon_Math_8393()
@@ -29,4 +32,44 @@ void Baekjoon_Math_2753()
 	}
 
 	std::cout << static_cast<int>(result) << std::endl;
+}
+
+/// 대학생 새내기들의 90%는 자신이 반에서 평균은 넘는다고 생각한다. 당신은 그들에게 슬픈 진실을 알려줘야 한다.
+/// 첫째 줄에는 테스트 케이스의 개수 C가 주어진다.
+/// 둘째 줄부터 각 테스트 케이스마다 학생의 수 N(1 ≤ N ≤ 1000, N은 정수)이 첫 수로 주어지고,
+///  이어서 N명의 점수가 주어진다.점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+/// 각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력한다.
+void Baekjoon_Math_4344()
+{
+	int C, N, inputBuffer;
+	double result, average;
+	std::vector<int> scores;
+	std::cin >> C;
+
+	for (int outerCounter = C; outerCounter > 0; outerCounter--)
+	{
+		scores.clear();
+		average = 0;
+		result = 0;
+		std::cin >> N;
+		for (int innerCounter = N; innerCounter > 0; innerCounter--)
+		{
+			std::cin >> inputBuffer;
+			scores.emplace_back(inputBuffer);
+		}
+
+		average = static_cast<double>(std::accumulate(scores.begin(), scores.end(), 0)) / static_cast<double>(scores.size());
+
+		for (const auto& elem : scores)
+		{
+			if (elem > average)
+			{
+				result++;
+			}
+		}
+
+		std::cout << std::fixed;
+		std::cout.precision(3);
+		std::cout << static_cast<double>(result) / static_cast<double>(scores.size()) * 100.0 << "%" << std::endl;
+	}
 }
