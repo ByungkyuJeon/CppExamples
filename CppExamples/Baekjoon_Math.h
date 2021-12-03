12cb3a330081332f361c13fd660c87b824803f3a
Byungkyu Jun <32653425+coxozo123@users.noreply.github.com>
2021-12-03 20:56:50 +0900

    added problem 2908

--- CppExamples/Baekjoon_Math.h
+++ CppExamples/Baekjoon_Math.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 #include <algorithm>
 #include <numeric>
 
@@ -73,3 +74,26 @@ void Baekjoon_Math_4344()
 		std::cout << static_cast<double>(result) / static_cast<double>(scores.size()) * 100.0 << "%" << std::endl;
 	}
 }
+
+/// 상근이의 동생 상수는 수학을 정말 못한다.상수는 숫자를 읽는데 문제가 있다.이렇게 수학을 못하는 상수를 위해서 상근이는 수의 크기를 비교하는 문제를 내주었다.상근이는 세 자리 수 두 개를 칠판에 써주었다.그 다음에 크기가 큰 수를 말해보라고 했다.
+/// 상수는 수를 다른 사람과 다르게 거꾸로 읽는다.예를 들어, 734와 893을 칠판에 적었다면, 상수는 이 수를 437과 398로 읽는다.따라서, 상수는 두 수중 큰 수인 437을 큰 수라고 말할 것이다.
+/// 두 수가 주어졌을 때, 상수의 대답을 출력하는 프로그램을 작성하시오.
+/// 첫째 줄에 상근이가 칠판에 적은 두 수 A와 B가 주어진다. 두 수는 같지 않은 세 자리 수이며, 0이 포함되어 있지 않다.
+void Beakjoon_Math_2908()
+{
+	int inputBuffer;
+	std::string strBuffer;
+	std::vector<int> nums;
+
+	for (int counter = 0; counter < 2; counter++)
+	{
+		strBuffer.clear();
+		std::cin >> inputBuffer;
+		strBuffer.append(std::to_string(inputBuffer));
+		std::swap(strBuffer[0], strBuffer[2]);
+		nums.emplace_back(std::stoi(strBuffer));
+	}
+		
+	std::cout << std::max(nums[0], nums[1]) << std::endl;
+}
+
