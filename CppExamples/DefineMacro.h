4b60e48957c98ca38c2aadeda7de119e148bae61
Byungkyu Jun <32653425+coxozo123@users.noreply.github.com>
2022-01-07 23:27:38 +0900

    added define maro example

---
+++ CppExamples/DefineMacro.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// #define
+//
+// is just changing code where it is used
+
+#define ADD_NO_BRACKET(x, y) x + y
+#define ADD(x, y) (x + y)
+
+#define EXP(x) (x * x)
+
+void executeDefineMacroExample()
+{
+	// 1. missing bracket problem
+	std::cout << ADD_NO_BRACKET(3, 5) << '\n'; // works fine
+	std::cout << ADD_NO_BRACKET(3, 5) * 2 << '\n'; // backet problem occur
+	std::cout << ADD(3, 5) * 2 << '\n'; // works properly
+
+	// 2. increment, decrement problem
+	int a = 4;
+	std::cout << EXP(a++) << '\n';
+
+	// 3. debugging problem
+}
